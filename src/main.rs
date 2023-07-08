8044ed16116520138e3f8e4a270973baadb1843e
trichoplax <xalpohcirt@gmail.com>
2023-07-08 16:03:50 +0100

    Add code to calculate the test cases

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use itertools::Itertools;
+
+fn main() {
+    let maximum_n = 10;
+
+    for value in 1..=maximum_n {
+        println!(
+            "{} : {:0<31}",
+            value,
+            format!("{}", probability_of_all_6_faces(value))
+        );
+    }
+}
+
+fn probability_of_all_6_faces(value: u64) -> f64 {
+    let denominator = 6_f64.powf(value as f64);
+    let multi_product = (1..=value).map(|_i| 1..=6).multi_cartesian_product();
+    let six_faced_results = multi_product.filter(|x| (1..=6).all(|y| x.contains(&y)));
+
+    six_faced_results.count() as f64 / denominator as f64
+}
